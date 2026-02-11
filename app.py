899fe9ed65fa4ab4c8b8efb32061bcdd58d7c47d
Maurya Suryakant <mauryasuryakant99@gmail.com>
2026-02-11 23:50:41 +0530

    Added notebook

--- app.py
+++ app.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 data = pd.read_csv("Data/data.csv")
 
+print("Original DataFrame")
+print(data.isnull().sum())
+
+print("\n")
+
+drop_null = data.dropna()
+
+dropped = pd.DataFrame(drop_null)
+
+dropped.to_csv("Data/data.csv", index = False)
+
 print(data)
+
+# data.dropna()
+# data.isnull().sum()
